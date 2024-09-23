e5acef291c11ddc6ef6cc2cf77fd044c4912f49d
abdalrhman <abdalrahmanabuyaqoub@gmail.com>
2024-09-23 16:58:38 +0300

    adding two color modes

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const board = document.querySelector(".board__container");
 const cellsContainer = document.querySelector(".flex__container");
 const cell = document.querySelector("[class^='cell']");
 const newGridButton = document.querySelector("#new__grid__button");
+const rgbButton = document.querySelector("#rgb__mode__button");
+const solidButton = document.querySelector("#solid__mode__button");
 const BOARD_WIDTH = 600;
 
 let size = 16;
 let height = BOARD_WIDTH / size;
+let colorMode = true;
 
 function createGrid() {
   for (let i = 1; i <= size * size; i++) {
@@ -18,14 +21,35 @@ function createGrid() {
 }
 createGrid();
 
-board.addEventListener("mouseover", (e) => {
+function getRandomColor() {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
 
+function setBackgroundColor(currentCell, color) {
+  if (currentCell.style.backgroundColor === board.style.backgroundColor) {
+    currentCell.style.backgroundColor = color;
+    currentCell.style.opacity = 0.1;
+    hasColor = true;
+  } else if (+currentCell.style.opacity < 1) {
+    currentCell.style.opacity = +currentCell.style.opacity + 0.1;
+  }
+  console.log(currentCell.style.backgroundColor);
+}
+
+board.addEventListener("mouseover", (e) => {
   let currentCell = e.target;
-  if (currentCell.className.includes("cell")) {
-    currentCell.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+  if (!currentCell.className.includes("cell")) return;
+
+  switch (colorMode) {
+    case true: //.......RGB
+      setBackgroundColor(currentCell, getRandomColor());
+      break;
+    case false: //....solid
+      setBackgroundColor(currentCell, "rgb(0,0,0)");
+      break;
   }
 });
 
@@ -41,4 +65,14 @@ newGridButton.addEventListener("click", () => {
   } else {
     alert("not a valid number");
   }
+});
+
+rgbButton.addEventListener("click", () => {
+  colorMode = true;
+  console.log(colorMode);
+});
+
+solidButton.addEventListener("click", () => {
+  colorMode = false;
+  console.log(colorMode);
 });
